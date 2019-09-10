f64f9da657f6ff40d0897f195ca59c512db89e39
Sharon Watkins <swatkins876@gmail.com>
2019-09-10 20:23:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,17 @@
 def using_include(array, element)
-array.include?(element)
+ array.include?(element)
 end
 
 def using_sort(array)
-array.sort
+ array.sort
 end
 
 def using_reverse(array)
-array.reverse 
+ array.reverse 
 end
 
 def using_first(array)
-
+ array.first 
 end
 
 def using_last(array)
